0b3336dd3f6ce781a4f3ccfe1894a25b674f9754
Victoria Meng <nights1ststar@protonmail.com>
2018-04-26 18:26:47 +0000

    automatically backed up by learn

--- bonus.sql
+++ bonus.sql
@@ -1,5 +1,5 @@
-INSERT INTO employees (name, bonus) VALUES ("Matthew", 1000);
-INSERT INTO employees (name, bonus) VALUES ("Abigail", 2000);
-INSERT INTO employees (name, bonus) VALUES ("Matthew", 500);
-INSERT INTO employees (name, bonus) VALUES ("Tom", 700);
-INSERT INTO employees (name, bonus) VALUES ("Abigail", 1250);
+INSERT INTO employees (employee, bonus) VALUES ("Matthew", 1000);
+INSERT INTO employees (employee, bonus) VALUES ("Abigail", 2000);
+INSERT INTO employees (employee, bonus) VALUES ("Matthew", 500);
+INSERT INTO employees (employee, bonus) VALUES ("Tom", 700);
+INSERT INTO employees (employee, bonus) VALUES ("Abigail", 1250);
