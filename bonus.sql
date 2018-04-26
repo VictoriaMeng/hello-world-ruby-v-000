2687a3b6b6c97a530dc8ba69b0ca4389a130a91c
Victoria Meng <nights1ststar@protonmail.com>
2018-04-26 18:22:55 +0000

    automatically backed up by learn

---
+++ bonus.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (name, bonus) VALUES ("Matthew", 1000);
+INSERT INTO employees (name, bonus) VALUES ("Abigail", 2000);
+INSERT INTO employees (name, bonus) VALUES ("Matthew", 500);
+INSERT INTO employees (name, bonus) VALUES ("Tom", 700);
+INSERT INTO employees (name, bonus) VALUES ("Abigail", 1250);
