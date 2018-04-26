c5b269084e79e06dc13d9124f91dac039ae86e20
Victoria Meng <nights1ststar@protonmail.com>
2018-04-26 18:12:01 +0000

    automatically backed up by learn

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO cats (id, name, age, breed, net_worth) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO cats (id, name, age, breed, net_worth) VALUES (2, "Hana", 1, "Tabby", 21800);
+INSERT INTO cats (id, name, age, breed, net_worth) VALUES (3, "Grumpy Cat", 4, "Persian", 181600);
+INSERT INTO cats (id, name, age, breed, net_worth) VALUES (4, "Lil\' Bub", 2, "Tortoiseshell", 2000000);
+
+INSERT INTO owners (name) VALUES ("mugumogu");
+INSERT INTO owners (name) VALUES ("Sophie");
+INSERT INTO owners (name) VALUES ("Penny");
+
+INSERT INTO cats_owners (cat_id, owner_id) VALUES (3, 2);
+INSERT INTO cats_owners (cat_id, owner_id) VALUES (3, 3);
+INSERT INTO cats_owners (cat_id, owner_id) VALUES (1, 2);
